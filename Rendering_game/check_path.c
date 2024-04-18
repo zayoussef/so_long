77a293c3f9833a302850e2a774db4c3bfd91121b
zayoussef <youssefweb09@gmail.com>
2024-04-18 17:16:06 +0100

    V_01

--- Rendering_game/check_path.c
+++ Rendering_game/check_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_path.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yozainan <yozainan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 03:26:53 by yozainan          #+#    #+#             */
-/*   Updated: 2024/04/18 16:20:44 by yozainan         ###   ########.fr       */
+/*   Updated: 2024/04/18 17:13:38 by youssef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,37 @@ void check_valid_path(char **map, int x, int y)
 {
     if (x < 0 || y < 0 || map[x][y] == '\0' 
     || map[x][y] == '1' || map[x][y] == 'Z')
-        return (0);
-    if (map[x][y] == '0' || map[x][y] == 'E')
+        return ;
+    if (map[x][y] == '0')
         map[x][y] = 'Z';
+    if (map[x][y] == 'E')
+    {
+        map[x][y] = 'Z';
+        return ;
+    }
     check_valid_path(map, x, y + 1);
     check_valid_path(map, x, y - 1);
     check_valid_path(map, x - 1, y);
     check_valid_path(map, x + 1, y);
+}
+
+int loop_maop(char **map)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if (map[i][j] == 'E' || map[i][j] == 'C')
+                return (0);
+            j++;
+        }
+        i++;
+    }
     return (1);
 }
 
@@ -54,12 +78,13 @@ int validation_path(char **map)
 
     player = find_position(map, 'P');
     copy_map = ft_copy_map(map);
-    if (!check_valid_path(copy_map, player->x, player->y))
-	{
+    check_valid_path(copy_map, player->x, player->y)
+	if (!loop_map(copy_map))
+    {
 		map_errors(4);
         free_map(copy_map);
 		exit(EXIT_FAILURE);
 	}
-    
-    
+    free_map(copy_map);
+    return (1);
 }
